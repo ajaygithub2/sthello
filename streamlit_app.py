a4cdab93a38af781eeb7e8ed5c5b0cf7524526ff
Ajay <115636212+ajaygithub2@users.noreply.github.com>
2023-04-12 12:13:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,3 @@
-import pygame
-import pygame.camera
-from pygame.locals import *
-import streamlit as st
+import cv2
 
-st.write("Hello Motherfuckers")
-
-pygame.init()
-pygame.camera.init()
-
-camlist = pygame.camera.list_cameras()
-if camlist:
-    cam = pygame.camera.Camera(camlist[0],(640,480))
-cam.start()
-
-image = cam.get_image()
-st.image(image)
+st.write("Hello")
